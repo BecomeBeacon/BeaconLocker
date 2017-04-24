1e09d685310f484a5e3e0d8af799e6664e26b9d4
gkatkdgur12 <gkatkdgur12@naver.com>
2017-04-24 22:10:20 +0900

    bluetooth start

---
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/KalmanFilter.java
@@ -0,0 +1,30 @@
+package com.example.becomebeacon.beaconlocker;
+
+/**
+ * Created by 함상혁입니다 on 2017-04-24.
+ */
+
+public class KalmanFilter {
+    private double X = 0;           // Filtered Value(���� ���Ⱚ)
+    private double Q = 0.00001;     // Process Noise
+    private double R = 0.001;       // Sensor Noise
+
+    private double P = 1;           // Estimated Error
+    private double K;               // Kalman Gain
+
+    KalmanFilter(double initValue) {
+        X = initValue;
+    }
+
+    private void measurementUpdate(){
+        K = (P + Q) / (P + Q + R);
+        P = R * (P + Q) / (R + P + Q);
+    }
+
+    public double update(double measurement){
+        measurementUpdate();
+        X = X + (measurement - X) * K;
+
+        return X;
+    }
+}
