4eba34e096e73e3a44a213615c5726c1f8348e2b
gwwork01 <gwwork01@gmail.com>
2017-05-02 02:28:00 +0900

    MainBugfix

--- app/src/main/java/com/example/becomebeacon/beaconlocker/MainActivity.java
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/MainActivity.java
@@ -280,8 +280,6 @@ public class MainActivity extends AppCompatActivity
                 .build();
         mGoogleApiClient.connect();
         super.onStart();
-
-        mDatabase = DataStoreActivity.getDatabase();
     }
 
     protected void onResume() {
