52ffb4705a1ac8aa259e70ed7a24997ff726fd3f
gkatkdgur12 <gkatkdgur12@naver.com>
2017-05-14 19:23:37 +0900

    beacon details

--- app/src/main/java/com/example/becomebeacon/beaconlocker/BeaconDetailsActivity.java
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/BeaconDetailsActivity.java
@@ -1,15 +1,26 @@
 package com.example.becomebeacon.beaconlocker;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.io.File;
 
 import static android.view.View.VISIBLE;
 
@@ -23,7 +34,16 @@ public class BeaconDetailsActivity extends AppCompatActivity {
     private TextView nickName;
     private TextView address;
     private TextView meter;
+    private Button disconnect;
+    private Button main;
+    private Button changeImage;
     static private BeaconDetailsActivity mContext;
+    private ImageView mImage;
+    private Bitmap mBitmap;
+    protected static final int CHOOSE_PICTURE = 0;
+    protected static final int TAKE_PICTURE = 1;
+    protected static Uri tempUri;
+    private static final int CROP_SMALL_PICTURE = 2;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +51,9 @@ public class BeaconDetailsActivity extends AppCompatActivity {
         mContext=this;
 
         item=DeviceInfoStore.getBleInfo();
-        nickName=(TextView)findViewById(R.id.et_NICKNAME);
-        address=(TextView)findViewById(R.id.et_Address);
-        meter=(TextView)findViewById(R.id.meter);
+        initUI();
+        initListeners();
+
 
         Log.d("BDA","item: "+item.nickname+", "+item.devAddress);
         nickName.setText(item.nickname);
@@ -44,6 +64,51 @@ public class BeaconDetailsActivity extends AppCompatActivity {
 
 
 
+    }
+
+
+    private void initUI() {
+        mImage= (ImageView) findViewById(R.id.imageView);
+        nickName=(TextView)findViewById(R.id.et_NICKNAME);
+        address=(TextView)findViewById(R.id.et_Address);
+        meter=(TextView)findViewById(R.id.meter);
+        disconnect=(Button)findViewById(R.id.disconnect);
+        main=(Button)findViewById(R.id.toMain);
+        changeImage=(Button)findViewById(R.id.changeImage);
+    }
+    private void initListeners() {
+        disconnect.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                BeaconList.mItemMap.remove(item.devAddress);
+                Log.d("BDA","size : "+BeaconList.mAssignedItem.size());
+                for(int i=0;i<BeaconList.mAssignedItem.size();i++)
+                {
+                    if(BeaconList.mAssignedItem.get(i).devAddress==item.devAddress) {
+                        BeaconList.mAssignedItem.remove(i);
+                        Log.d("BDA","removed");
+
+                    }
+                }
+
+                //서버에서도 없애줘야한다 무조건
+            }
+        });
+
+        main.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+
+        changeImage.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                //이부분 새로운 사진어쩌구 쓴다
+                showChoosePicDialog();
+            }
+        });
     }
 
     @Override
@@ -62,6 +127,89 @@ public class BeaconDetailsActivity extends AppCompatActivity {
     public void refreshDistance()
     {
         meter.setText(String.format("%.2f",item.distance2));
+    }
+
+    protected void showChoosePicDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(BeaconDetailsActivity.this);
+        builder.setTitle("사진선택");
+        String[] items = { "사진 선택하기", "카메라" };
+        builder.setNegativeButton("취소", null);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case CHOOSE_PICTURE: // 사진 선택
+                        Intent openAlbumIntent = new Intent(
+                                Intent.ACTION_GET_CONTENT);
+                        openAlbumIntent.setType("image/*");
+                        //startActivityForResult사용한다.
+                        startActivityForResult(openAlbumIntent, CHOOSE_PICTURE);
+                        break;
+                    case TAKE_PICTURE: // 카메라
+                        Intent openCameraIntent = new Intent(
+                                MediaStore.ACTION_IMAGE_CAPTURE);
+                        tempUri = Uri.fromFile(new File(Environment
+                                .getExternalStorageDirectory(), "temp_image.jpg"));
+                        // 카메라 찍은사진은 SD에 저장
+                        openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri);
+                        startActivityForResult(openCameraIntent, TAKE_PICTURE);
+                        break;
+                }
+            }
+        });
+        builder.show();
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == MainActivity.RESULT_OK) {
+            switch (requestCode) {
+                case TAKE_PICTURE:
+                    cutImage(tempUri); // 사진 마름질하다.
+                    break;
+                case CHOOSE_PICTURE:
+                    cutImage(data.getData());
+                    break;
+                case CROP_SMALL_PICTURE:
+                    if (data != null) {
+                        setImageToView(data); // 사진은 미리보기
+                    }
+                    break;
+            }
+        }
+    }
+    /**
+     * 사진 마름질하다.
+     */
+    protected void cutImage(Uri uri) {
+        if (uri == null) {
+            Log.i("alanjet", "The uri is not exist.");
+        }
+        tempUri = uri;
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        // 설정
+        intent.putExtra("crop", "true");
+        // aspectX aspectY
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, CROP_SMALL_PICTURE);
+    }
+    /**
+     * 사진 저장
+     */
+    protected void setImageToView(Intent data) {
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            mBitmap = extras.getParcelable("data");
+            //사진은 사각형
+            mImage.setImageBitmap(mBitmap);//미리보기...
+        }
     }
 
 
