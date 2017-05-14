aca82a21a5e1b894ce143f8e0321d3ac8072e25d
gkatkdgur12 <gkatkdgur12@naver.com>
2017-05-14 22:43:59 +0900

    Merge branch 'master' of https://github.com/becomebeacon/beaconlocker
    # Conflicts:
    #	app/src/main/java/com/example/becomebeacon/beaconlocker/BeaconDetailsActivity.java

--- app/src/main/java/com/example/becomebeacon/beaconlocker/SettingActivity.java
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/SettingActivity.java
@@ -4,6 +4,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -28,10 +30,24 @@ public class SettingActivity extends AppCompatActivity {
 
         SharedPreferences pref = getSharedPreferences("pref", AppCompatActivity.MODE_PRIVATE); // Shared Preference를 불러옵니다.
 
+        scanOnOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                    Values.useBLE=true;
+                }
+                else
+                {
+                    Values.useBLE=false;
+                }
+            }
+        });
 
         // 저장된 값들을 불러옵니다.
-        float scanTime = pref.getFloat("ScanPeriod", (float)5.0);
+        float scanTime = pref.getInt("ScanPeriod", 5);
         Boolean useScan = pref.getBoolean("UseScan", true);
+
 
         scanOnOff.setChecked(useScan);
         scanPeriod.setText(""+scanTime);
@@ -46,9 +62,12 @@ public class SettingActivity extends AppCompatActivity {
 
         SharedPreferences.Editor editor = pref.edit(); // Editor를 불러옵니다.
 
+        Values.scanBreakTime=Integer.valueOf(scanPeriod.getText().toString());
+
+
         // 저장할 값들을 입력합니다.
-        editor.putFloat("ScanPeriod", Float.valueOf(scanPeriod.getText().toString()));
-        editor.putBoolean("UseScan1", scanOnOff.isChecked());
+        editor.putInt("ScanPeriod", Integer.valueOf(scanPeriod.getText().toString()));
+        editor.putBoolean("UseScan", scanOnOff.isChecked());
 
         editor.commit();
 
