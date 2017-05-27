349f905bc322557d5681966146930a4f06b763d1
gwmail <gwwork01@gmail.com>
2017-05-27 22:02:21 +0900

    Data Delete(Complete)

--- app/src/main/java/com/example/becomebeacon/beaconlocker/DataStoreActivity.java
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/DataStoreActivity.java
@@ -186,7 +186,7 @@ public class DataStoreActivity extends AppCompatActivity {
         BleDeviceInfo bleDeviceInfo = DeviceInfoStore.getBleInfo();
         BeaconOnUser beaconOnUser = new BeaconOnUser(bleDeviceInfo.getDevAddress());
 
-        mUserAddressRef.child(bleDeviceInfo.getDevAddress() + "/").setValue(beaconOnUser);
+        mUserAddressRef.child(bleDeviceInfo.getDevAddress()).setValue(beaconOnUser);
 
         //store beacon info to 'Beacon' DB in Uid order
         bleDeviceInfo.setNickname(et_Nickname.getText().toString());
