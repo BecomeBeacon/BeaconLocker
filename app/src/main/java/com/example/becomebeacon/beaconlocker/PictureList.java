42ef397a38c0ec3aba9570f252f57570217c07bf
Enzyu <zero2684@gmail.com>
2017-05-29 20:50:38 +0900

    21251

---
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/PictureList.java
@@ -0,0 +1,13 @@
+package com.example.becomebeacon.beaconlocker;
+
+import android.graphics.Bitmap;
+
+import java.util.HashMap;
+
+/**
+ * Created by Ryu on 2017-05-27.
+ */
+
+public class PictureList {
+    static public HashMap<String,Bitmap> pictures=new HashMap<>();
+}
