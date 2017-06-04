a0fb0db7e69cd05d3b97b67232fbb0cc3af22b82
gwmail <gwwork01@gmail.com>
2017-06-04 17:29:12 +0900

    Merge branch 'master' of https://github.com/BecomeBeacon/BeaconLocker
    # Conflicts:
    #	app/src/main/java/com/example/becomebeacon/beaconlocker/BeaconDetailsActivity.java
    #	app/src/main/java/com/example/becomebeacon/beaconlocker/Callback.java
    #	app/src/main/java/com/example/becomebeacon/beaconlocker/DataStoreActivity.java

--- app/src/main/java/com/example/becomebeacon/beaconlocker/BeaconDetailsActivity.java
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/BeaconDetailsActivity.java
@@ -26,6 +26,7 @@ import android.widget.Toast;
 
 import com.example.becomebeacon.beaconlocker.pictureserver.Callback;
 import com.example.becomebeacon.beaconlocker.pictureserver.PictureDelete;
+import com.example.becomebeacon.beaconlocker.pictureserver.PicturePopup;
 import com.example.becomebeacon.beaconlocker.pictureserver.PictureUpload;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
@@ -51,7 +52,7 @@ public class BeaconDetailsActivity extends AppCompatActivity {
     private Button main;
     private Button changeImage;
     static private BeaconDetailsActivity mContext;
-    private ImageView mImage;
+    private ImageView ivPreview;
     private Bitmap mBitmap;
     protected static final int CHOOSE_PICTURE = 0;
     protected static final int TAKE_PICTURE = 1;
@@ -64,6 +65,8 @@ public class BeaconDetailsActivity extends AppCompatActivity {
 
     private Uri filePath = null;
     private ProgressDialog progressDialog = null;
+
+    private PicturePopup picturePopup;
 
 
 
@@ -108,7 +111,7 @@ public class BeaconDetailsActivity extends AppCompatActivity {
     }
 
     private void initUI() {
-        mImage= (ImageView) findViewById(R.id.iv_image);
+        ivPreview= (ImageView) findViewById(R.id.iv_image);
         nickName=(EditText)findViewById(R.id.et_NICKNAME);
         address=(TextView)findViewById(R.id.et_address);
         meter=(TextView)findViewById(R.id.meter);
@@ -218,8 +221,7 @@ public class BeaconDetailsActivity extends AppCompatActivity {
         changeImage.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v)
             {
-                //이부분 새로운 사진어쩌구 쓴다
-                showChoosePicDialog();
+                uploadMyPictureDialog();
             }
         });
 
@@ -265,6 +267,52 @@ public class BeaconDetailsActivity extends AppCompatActivity {
         }
     }
 
+    public void fetchPicture() {
+        Bitmap bitmapImage=PictureList.pictures.get(item.devAddress);
+
+        ivPreview.setImageBitmap(bitmapImage);
+    }
+
+    ////////////////사진 팝업 및 저장 관련 메소드 ////////////////////
+    private void uploadMyPictureDialog() {
+        //이미지를 선택
+        picturePopup = new PicturePopup(BeaconDetailsActivity.this);
+        picturePopup.showChoosePicDialog(new Callback() {
+            @Override
+            public void callBackMethod(Object obj) {
+                //사진 선택
+                startActivityForResult((Intent)obj, CHOOSE_PICTURE);
+            }
+        }, new Callback() {
+            @Override
+            public void callBackMethod(Object obj) {
+                //사진 촬영
+                startActivityForResult((Intent)obj, TAKE_PICTURE);
+            }
+        });
+    }
+
+    //Picture Intent 생성 후 Result 처리
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == DataStoreActivity.RESULT_OK) {
+            picturePopup.pictureActivityForResult(requestCode, data, new Callback() {
+                @Override
+                public void callBackMethod(Object obj) {
+                    try {
+                        filePath = (Uri)obj;
+                        //Uri 파일을 Bitmap으로 만들어서 ImageView에 집어 넣는다.
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                        ivPreview.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
     static public BeaconDetailsActivity getBDA()
     {
         return mContext;
@@ -275,87 +323,87 @@ public class BeaconDetailsActivity extends AppCompatActivity {
         meter.setText(String.format("%.2f",item.distance2));
     }
 
-    protected void showChoosePicDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(BeaconDetailsActivity.this);
-        builder.setTitle("사진선택");
-        String[] items = { "사진 선택하기", "카메라" };
-        builder.setNegativeButton("취소", null);
-        builder.setItems(items, new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case CHOOSE_PICTURE: // 사진 선택
-                        Intent openAlbumIntent = new Intent(
-                                Intent.ACTION_GET_CONTENT);
-                        openAlbumIntent.setType("image/*");
-                        //startActivityForResult사용한다.
-                        startActivityForResult(openAlbumIntent, CHOOSE_PICTURE);
-                        break;
-                    case TAKE_PICTURE: // 카메라
-                        Intent openCameraIntent = new Intent(
-                                MediaStore.ACTION_IMAGE_CAPTURE);
-                        tempUri = Uri.fromFile(new File(Environment
-                                .getExternalStorageDirectory(), "temp_image.jpg"));
-                        // 카메라 찍은사진은 SD에 저장
-                        openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri);
-                        startActivityForResult(openCameraIntent, TAKE_PICTURE);
-                        break;
-                }
-            }
-        });
-        builder.show();
-    }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == MainActivity.RESULT_OK) {
-            Log.d("BeaconDetailsAct", "버튼 클릭");
-            int result = new PermissionRequester.Builder(BeaconDetailsActivity.this)
-                    .setTitle("권한 요청")
-                    .setMessage("권한을 요청합니다.")
-                    .setPositiveButtonName("네")
-                    .setNegativeButtonName("아니요.")
-                    .create()
-                    .request(Manifest.permission.CALL_PHONE, 1000 , new PermissionRequester.OnClickDenyButtonListener() {
-                        @Override
-                        public void onClick(Activity activity) {
-                            Log.d("RESULT", "취소함.");
-                        }
-                    });
-
-            if (result == PermissionRequester.ALREADY_GRANTED) {
-                Log.d("RESULT", "권한이 이미 존재함.");
-                if (ActivityCompat.checkSelfPermission(BeaconDetailsActivity.this,
-                        Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-                }
-            }
-            else if(result == PermissionRequester.NOT_SUPPORT_VERSION)
-                Log.d("RESULT", "마쉬멜로우 이상 버젼 아님.");
-            else if(result == PermissionRequester.REQUEST_PERMISSION) {
-                Log.d("RESULT", "요청함. 응답을 기다림.");
-                switch (requestCode) {
-                    case TAKE_PICTURE:
-                        //cutImage(tempUri); // 사진 마름질하다.
-                        filePath = tempUri;
-                        imageToView(filePath);
-                        Log.v("Test", "filepath = " + filePath);
-                        break;
-                    case CHOOSE_PICTURE:
-                        //cutImage(data.getData());
-                        filePath = data.getData();
-                        imageToView(filePath);
-                        Log.v("Test", "filepath = " + filePath);
-                        break;
-                    case CROP_SMALL_PICTURE:
-                        if (data != null) {
-                            setImageToView(data); // 사진은 미리보기
-                        }
-                        break;
-                }
-            }
-        }
-    }
+//    protected void showChoosePicDialog() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(BeaconDetailsActivity.this);
+//        builder.setTitle("사진선택");
+//        String[] items = { "사진 선택하기", "카메라" };
+//        builder.setNegativeButton("취소", null);
+//        builder.setItems(items, new DialogInterface.OnClickListener() {
+//
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                switch (which) {
+//                    case CHOOSE_PICTURE: // 사진 선택
+//                        Intent openAlbumIntent = new Intent(
+//                                Intent.ACTION_GET_CONTENT);
+//                        openAlbumIntent.setType("image/*");
+//                        //startActivityForResult사용한다.
+//                        startActivityForResult(openAlbumIntent, CHOOSE_PICTURE);
+//                        break;
+//                    case TAKE_PICTURE: // 카메라
+//                        Intent openCameraIntent = new Intent(
+//                                MediaStore.ACTION_IMAGE_CAPTURE);
+//                        tempUri = Uri.fromFile(new File(Environment
+//                                .getExternalStorageDirectory(), "temp_image.jpg"));
+//                        // 카메라 찍은사진은 SD에 저장
+//                        openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri);
+//                        startActivityForResult(openCameraIntent, TAKE_PICTURE);
+//                        break;
+//                }
+//            }
+//        });
+//        builder.show();
+//    }
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == MainActivity.RESULT_OK) {
+//            Log.d("BeaconDetailsAct", "버튼 클릭");
+//            int result = new PermissionRequester.Builder(BeaconDetailsActivity.this)
+//                    .setTitle("권한 요청")
+//                    .setMessage("권한을 요청합니다.")
+//                    .setPositiveButtonName("네")
+//                    .setNegativeButtonName("아니요.")
+//                    .create()
+//                    .request(Manifest.permission.CALL_PHONE, 1000 , new PermissionRequester.OnClickDenyButtonListener() {
+//                        @Override
+//                        public void onClick(Activity activity) {
+//                            Log.d("RESULT", "취소함.");
+//                        }
+//                    });
+//
+//            if (result == PermissionRequester.ALREADY_GRANTED) {
+//                Log.d("RESULT", "권한이 이미 존재함.");
+//                if (ActivityCompat.checkSelfPermission(BeaconDetailsActivity.this,
+//                        Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+//                }
+//            }
+//            else if(result == PermissionRequester.NOT_SUPPORT_VERSION)
+//                Log.d("RESULT", "마쉬멜로우 이상 버젼 아님.");
+//            else if(result == PermissionRequester.REQUEST_PERMISSION) {
+//                Log.d("RESULT", "요청함. 응답을 기다림.");
+//                switch (requestCode) {
+//                    case TAKE_PICTURE:
+//                        //cutImage(tempUri); // 사진 마름질하다.
+//                        filePath = tempUri;
+//                        imageToView(filePath);
+//                        Log.v("Test", "filepath = " + filePath);
+//                        break;
+//                    case CHOOSE_PICTURE:
+//                        //cutImage(data.getData());
+//                        filePath = data.getData();
+//                        imageToView(filePath);
+//                        Log.v("Test", "filepath = " + filePath);
+//                        break;
+//                    case CROP_SMALL_PICTURE:
+//                        if (data != null) {
+//                            setImageToView(data); // 사진은 미리보기
+//                        }
+//                        break;
+//                }
+//            }
+//        }
+//    }
     /**
      * 사진 마름질하다.
      */
@@ -385,25 +433,7 @@ public class BeaconDetailsActivity extends AppCompatActivity {
         if (extras != null) {
             mBitmap = extras.getParcelable("data");
             //사진은 사각형
-            mImage.setImageBitmap(mBitmap);//미리보기...
+            ivPreview.setImageBitmap(mBitmap);//미리보기...
         }
-    }
-    private void imageToView(Uri uri) {
-        try {
-            //Uri 파일을 Bitmap으로 만들어서 ImageView에 집어 넣는다.
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-            mImage.setImageBitmap(bitmap);
-            PictureList.pictures.remove(item.devAddress);
-            PictureList.pictures.put(item.devAddress,bitmap);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void fetchPicture() {
-        Bitmap bitmapImage=PictureList.pictures.get(item.devAddress);
-
-        mImage.setImageBitmap(bitmapImage);
     }
 }
