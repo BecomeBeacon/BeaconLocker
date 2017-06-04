42fa14238b93b2feeef5b8e864303984bac539fc
gkatkdgur12 <gkatkdgur12@naver.com>
2017-06-04 10:23:45 +0900

    Merge branch 'master' of https://github.com/becomebeacon/beaconlocker
    # Conflicts:
    #	app/src/main/java/com/example/becomebeacon/beaconlocker/BleService.java

--- app/src/main/java/com/example/becomebeacon/beaconlocker/CallBack.java
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/CallBack.java
@@ -6,6 +6,6 @@ import java.io.Serializable;
  * Created by gwmail on 2017-06-03.
  */
 
-public interface Callback extends Serializable {
+public interface CallBack extends Serializable {
     void callBackMethod(Object obj);
 }
